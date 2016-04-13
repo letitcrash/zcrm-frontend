9d44fb5a5c938ef28aa5880a26ea645098de48ea
Pavel Smal <pavel.smal@gmail.com>
2016-04-13 17:15:44 +0300

    Added Outbox to mail

--- src/app/mail/mailboxController.js
+++ src/app/mail/mailboxController.js
@@ -1,9 +1,9 @@
 'use strict';
 angular.module('inspinia').controller("MailboxCtrl", function($scope, $rootScope, $state, mailboxService) {
-  var getMail;
+  var getMail,getOutbox;
 
   getMail = function(force, pageSize, pageNr, searchTerm) {
-   mailboxService.getList(force, pageSize, pageNr, searchTerm).then(function(response) {
+   mailboxService.getInbox(force, pageSize, pageNr, searchTerm).then(function(response) {
       console.log("got mail");
       console.log(response);
       $scope.showLoadingMessage = false;
@@ -14,16 +14,57 @@ angular.module('inspinia').controller("MailboxCtrl", function($scope, $rootScope
        console.log(response);
     });
   };
+  
+  getOutbox = function(force, pageSize, pageNr, searchTerm) {
+   mailboxService.getOutbox(force, pageSize, pageNr, searchTerm).then(function(response) {
+      console.log("got mail");
+      console.log(response);
+      $scope.showLoadingMessage = false;
+      $scope.mail = response;
+      $scope.totalItems = response.totalSize;
+    }, function(response) {
+      console.log("Could not get employees");
+       console.log(response);
+    });
+  };
+
   $scope.showInbox = function() {
     $scope.activeMail = false;
     $scope.compose = false;
 
     getMail(false, $scope.pageSize, $scope.pageNr, $scope.searchTerm);
   };
+
+  $scope.showOutbox = function() {
+    $scope.activeMail = false;
+    $scope.compose = false;
+
+    getOutbox(false, $scope.pageSize, $scope.pageNr, $scope.searchTerm);
+  };
+
   $scope.composeMail = function() {
     $scope.activeMail = false;
     $scope.compose = true;
   };
+
+  $scope.sendMail = function(email, recp) {
+    email.to = [];
+    email.to.push(recp);
+    console.log(email);
+
+    mailboxService.post(email).then(function(response) {
+      console.log("sent mail");
+      console.log(response);
+     
+    }, function(response) {
+      console.log("Could not sent mail");
+       console.log(response);
+    });
+
+    $scope.activeMail = false;
+    $scope.compose = true;
+  };
+
   $scope.showMail = function(mail) {
     $scope.activeMail = true;
     $scope.currentMail = mail;
--- src/app/services/mailboxService.js
+++ src/app/services/mailboxService.js
@@ -1,9 +1,9 @@
 'use strict';
 angular.module('inspinia').factory('mailboxService', function(requestService, dataService) {
   return {
-    getList: function(force, pageSize, pageNr, searchTerm) {
+    getInbox: function(force, pageSize, pageNr, searchTerm) {
       var url;
-        url = "companies/" + dataService.getCurrentCompanyId() + "/employees/" + dataService.getEmployments().id + "/mails";
+        url = "companies/" + dataService.getCurrentCompanyId() + "/employees/" + dataService.getEmployments().id + "/mail/inbox";
       if (pageSize && pageNr) {
         if (searchTerm) {
           url = url + "?pageSize=" + pageSize + "&pageNr=" + pageNr + "&searchTerm=" + searchTerm;
@@ -12,6 +12,23 @@ angular.module('inspinia').factory('mailboxService', function(requestService, da
         }
       }
       return requestService.ttGet(url);
+    },
+    getOutbox: function(force, pageSize, pageNr, searchTerm) {
+      var url;
+        url = "companies/" + dataService.getCurrentCompanyId() + "/employees/" + dataService.getEmployments().id + "/mail/sentbox";
+      if (pageSize && pageNr) {
+        if (searchTerm) {
+          url = url + "?pageSize=" + pageSize + "&pageNr=" + pageNr + "&searchTerm=" + searchTerm;
+        } else {
+          url = url + "?pageSize=" + pageSize + "&pageNr=" + pageNr;
+        }
+      }
+      return requestService.ttGet(url);
+    },
+    post: function(email) {
+      var url;
+        url = "companies/" + dataService.getCurrentCompanyId() + "/employees/" + dataService.getEmployments().id + "/mail/send";
+      return requestService.ttPost(url, email);
     }
   };
 });
