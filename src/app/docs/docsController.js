5fe2739104910f56e1e48cb2a6e29bf8094af030
Evgeny Yulin <alltiptop1@gmail.com>
2016-05-25 17:57:00 +0400

    #6 return upload

--- src/app/docs/docsController.js
+++ src/app/docs/docsController.js
@@ -49,6 +49,47 @@ angular.module('inspinia')
   }
   ];
   $scope.files = files;
+  
+  $scope.uploadFiles = function(files, errFiles) {
+	$scope.files = files;
+	$scope.errFiles = errFiles;
+	angular.forEach(files, function(file) {
+	  file.upload = Upload.upload({
+		url: upUrl,
+		data: {fileUpload: file}
+	  });
+	  
+	  file.upload.then(function (response) {
+		$timeout(function () {
+		  console.log(response)
+		  file.result = response.data;
+		});
+	  }, function (response) {
+		if (response.status > 0)
+		  $scope.errorMsg = response.status + ': ' + response.data;
+	  }, function (evt) {
+		console.log(evt);
+		file.progress = Math.min(100, parseInt(100.0 * 
+		evt.loaded / evt.total));
+		console.log(file.progress);
+	  });
+	});
+  }
+  
+  $scope.getUserFiles = function() {
+	//docsService.get(dataService.getEmployments().id).then(function(response) {
+	$http.get(upUrl).then(function(response) {
+	  console.log("got files list");
+	  console.log(response);
+	  $scope.files = response.data;
+	  $scope.showLoadingMessage = false;
+	}, function(response) {
+	  console.log("Could not get files");
+	  console.log(response);
+	});
+  };
+  $scope.getUserFiles();
+
 });
 
 angular.module('ui.bootstrap').controller('Collapse', function ($scope) {
