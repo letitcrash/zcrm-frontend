c2408b832996c5e9971a712b377d5d5b24a95c3b
Evgeny Yulin <alltiptop1@gmail.com>
2016-06-03 17:44:17 +0400

    employee table fixes

--- src/app/companies/companyDirective.js
+++ src/app/companies/companyDirective.js
@@ -5,7 +5,7 @@ angular.module('inspinia').directive('cpElement', function () {
       onEdit: '&'
     },
     restrict: 'EA',
-    template: '<td style="width:35px;"> <input type="checkbox" style="margin-left:10px;" name="vehicle" value="Car"></td><td><a ng-click="onEdit({cp:item})" class="client-link">{{item.name}} </a></td><td>{{item.contactProfile.firstname}} {{item.contactProfile.lastname}}</td><td>{{item.contactProfile.email}}</td>'
+	template: '<td style="width:35px;"> <input class="green-check" type="checkbox" style="margin-left:10px;" name="vehicle" value="Car"></td><td><a ng-click="onEdit({cp:item})" class="client-link">{{item.name}} </a></td><td>{{item.contactProfile.firstname}} {{item.contactProfile.lastname}}</td><td>{{item.contactProfile.email}}</td>'
     };
 });
 
