891569e80886508c756acd98f983abc7ddc580cb
sgflt <sgflt88@gmail.com>
2016-06-03 19:02:00 +0300

    Conversation tab modified, navigation and comments added

--- src/app/tickets/ticketsController.js
+++ src/app/tickets/ticketsController.js
@@ -4,25 +4,22 @@ angular.module('inspinia').controller("TicketsCtrl", function($scope, $rootScope
     
     //inserted
     $scope.selected = 1;
-    $scope.issues = [
-        {issueName: "ISSUE-1", issueCreatedBy: "Monica", issueDate: "May 13, 2016"},
-        {issueName: "ISSUE-2", issueCreatedBy: "Jake", issueDate: "May 13, 2016" },
-        {issueName: "ISSUE-3", issueCreatedBy: "Flygon", issueDate: "May 13, 2016" }
-    ];
+
     $scope.itable = [
-                    {update:'new', priority: 'high', number: 'Issue-1', client: 'Client-1', team: 'Team1', person: 'person1@gmail.com', created: '13.05.2016', deadline: 'Today'},
-                    {update:'overdue', priority: 'mid', number: 'Issue-2', client: 'Client-2', team: 'Team2', person: 'person2@gmail.com', created: '13.05.2016', deadline: 'Today'},
-                    {update:'closed', priority: 'low', number: 'Issue-3', client: 'Client-3', team: 'Team3', person: 'person3@gmail.com', created: '13.05.2016', deadline: 'Today'}, 
-                    {update:'new', priority: 'high', number: 'Issue-1', client: 'Client-1', team: 'Team1', person: 'person1@gmail.com', created: '13.05.2016', deadline: 'Today'},
-                    {update:'overdue', priority: 'mid', number: 'Issue-2', client: 'Client-2', team: 'Team2', person: 'person2@gmail.com', created: '13.05.2016', deadline: 'Today'},
-                    {update:'closed', priority: 'low', number: 'Issue-3', client: 'Client-3', team: 'Team3', person: 'person3@gmail.com', created: '13.05.2016', deadline: 'Today'},
-                    {update:'new', priority: 'high', number: 'Issue-1', client: 'Client-1', team: 'Team1', person: 'person1@gmail.com', created: '13.05.2016', deadline: 'Today'},
-                    {update:'overdue', priority: 'mid', number: 'Issue-2', client: 'Client-2', team: 'Team2', person: 'person2@gmail.com', created: '13.05.2016', deadline: 'Today'},
-                    {update:'closed', priority: 'low', number: 'Issue-3', client: 'Client-3', team: 'Team3', person: 'person3@gmail.com', created: '13.05.2016', deadline: 'Today'},
-                     {update:'new', priority: 'high', number: 'Issue-1', client: 'Client-1', team: 'Team1', person: 'person1@gmail.com', created: '13.05.2016', deadline: 'Today'},
-                    {update:'overdue', priority: 'mid', number: 'Issue-2', client: 'Client-2', team: 'Team2', person: 'person2@gmail.com', created: '13.05.2016', deadline: 'Today'},
-                    {update:'closed', priority: 'low', number: 'Issue-3', client: 'Client-3', team: 'Team3', person: 'person3@gmail.com', created: '13.05.2016', deadline: 'Today'}
+                    {update:'new', priority: 'high', number: 'Issue-1', client: 'Client-1', team: 'Team-1', person: 'person1@gmail.com', created: '1288323623006', deadline: '1288323623006', description: 'Capicola bresaola shank beef ribs. Pancetta boudin jowl, sausage beef ball tip turducken pork salami t-bone brisket kielbasa kevin ham venison. Landjaeger turducken shankle, jerky biltong hamburger leberkas meatloaf pork boudin swine bacon tri-tip shank andouille. Doner corned beef frankfurter capicola biltong tri-tip ball tip porchetta drumstick chicken meatball jowl t-bone. Leberkas pork chop chuck, shankle bresaola pork loin porchetta meatloaf corned beef tongue.'},
+                    {update:'overdue', priority: 'mid', number: 'Issue-2', client: 'Client-2', team: 'Team-2', person: 'person2@gmail.com', created: '1288323623006', deadline: '1288323623006'},
+                    {update:'closed', priority: 'low', number: 'Issue-3', client: 'Client-3', team: 'Team-3', person: 'person3@gmail.com', created: '1288323623006', deadline: '1288323623006'}, 
+                    {update:'new', priority: 'high', number: 'Issue-1', client: 'Client-1', team: 'Team-1', person: 'person1@gmail.com', created: '1288323623006', deadline: '1288323623006'},
+                    {update:'overdue', priority: 'mid', number: 'Issue-2', client: 'Client-2', team: 'Team-2', person: 'person2@gmail.com', created: '1288323623006', deadline: '1288323623006'},
+                    {update:'closed', priority: 'low', number: 'Issue-3', client: 'Client-3', team: 'Team-3', person: 'person3@gmail.com', created: '1288323623006', deadline: '1288323623006'},
+                    {update:'new', priority: 'high', number: 'Issue-1', client: 'Client-1', team: 'Team-1', person: 'person1@gmail.com', created: '1288323623006', deadline: '1288323623006'},
+                    {update:'overdue', priority: 'mid', number: 'Issue-2', client: 'Client-2', team: 'Team-2', person: 'person2@gmail.com', created: '1288323623006', deadline: '1288323623006'},
+                    {update:'closed', priority: 'low', number: 'Issue-3', client: 'Client-3', team: 'Team-3', person: 'person3@gmail.com', created: '1288323623006', deadline: '1288323623006'},
+                     {update:'new', priority: 'high', number: 'Issue-1', client: 'Client-1', team: 'Team-1', person: 'person1@gmail.com', created: '1288323623006', deadline: '1288323623006'},
+                    {update:'overdue', priority: 'mid', number: 'Issue-2', client: 'Client-2', team: 'Team-2', person: 'person2@gmail.com', created: '1288323623006', deadline: '1288323623006'},
+                    {update:'closed', priority: 'low', number: 'Issue-3', client: 'Client-3', team: 'Team-3', person: 'person3@gmail.com', created: '1288323623006', deadline: '1288323623006'}
                 ];
+
     //inserted
     
   getTickets = function(force, pageSize, pageNr, searchTerm) {
