19664ec12e8abe2a226a0d723aeff4f8c1bdff0a
paulsmal <pavel.smal@gmail.com>
2016-06-09 18:35:01 +0300

    files fixed

--- src/app/docs/docsController.js
+++ src/app/docs/docsController.js
@@ -10,7 +10,7 @@ angular.module('inspinia')
 	  console.log("got files");
 	  console.log(response);
 	  $scope.showLoadingMessage = false;
-	  $scope.files = response;
+	  $scope.uploadedFiles = response.data;
 	  $scope.totalItems = response.totalCount;
 	}, function(response) {
 	  console.log("Could not get files");
