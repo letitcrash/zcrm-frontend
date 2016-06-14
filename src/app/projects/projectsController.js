a087c6c730cf9226679d46175a4bd53b7dae568b
Andrew <andrewboltiansky@gmail.com>
2016-06-14 15:48:16 +0300

    Added View page to projects

--- src/app/projects/projectsController.js
+++ src/app/projects/projectsController.js
@@ -13,12 +13,59 @@ angular.module('inspinia').controller("ProjectsCtrl", function($scope, $rootScop
     });
   };
 
+  
+
+  $scope.openProject = function(proj) {
+      console.log(proj)
+      if($scope.page.mode == 0) {
+        $scope.page.mode = 1;
+        console.log($scope.page.mode);
+      }
+      $scope.currentProject = angular.copy(proj);
+  }
+
+  $scope.defaultMode = function () {
+    $scope.page.mode = 0;
+    console.log($scope.page.mode);
+  };
+
+  $scope.compressMode = function () {
+    $scope.page.mode = 1;
+  };
+
+  $scope.expandMode = function () {
+    $scope.page.mode = 2;
+  };
+
+
+  $scope.closeCurrentProject = function() {
+    $scope.page.mode = 0;
+
+  };
+
+  $scope.createProjectAction = function () {
+    $scope.compressMode();
+    $scope.currentProject = {};
+    $scope.currentProject.members = [];
+    $scope.currentProject.id = null;
+
+  };
+
   $scope.init = function() {
     console.log("Running init in Projects Controller");
     getProjects(false, $scope.pageSize, $scope.pageNr, $scope.searchTerm);
     $scope.page = {};
     $scope.page.mode = 0;
+
+    $scope.isCollapsed = false;
+    $scope.pageSize = 10;
+    $scope.pageNr = 1;
+    $scope.searchTerm = "";
+    $scope.currentProject= {};
+    $scope.currentProject.members = [];
   };
+
+
     
   $scope.init();
 
