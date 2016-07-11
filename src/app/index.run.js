be9297627f35105bd3753d2a9c006b26400e322f
paulsmal <pavel.smal@gmail.com>
2016-07-11 15:20:04 +0300

    backend url according to env set

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -9,8 +9,19 @@
   /** @ngInject */
   function initConstants($log, $rootScope, dataService, mailboxService) {
 
+    if (window.location.protocol === "https:" && window.location.host === "desk-it.com") {
+      dataService.setBaseServiceURL('https://desk-it.com:9443/');
+    } else if (window.location.protocol === "https:" && window.location.host === "uat.desk-it.com") {
+      dataService.setBaseServiceURL('https://uat.desk-it.com:10443/');
+    } else if (window.location.protocol === "http:" && window.location.host === "desk-it.com") {
+      dataService.setBaseServiceURL('http://desk-it.com:9000/');
+    } else if (window.location.protocol === "http:" && window.location.host === "uat.desk-it.com") {
+      dataService.setBaseServiceURL('http://uat.desk-it.com:10000/');
+    } else {
+      dataService.setBaseServiceURL('https://desk-it.com:9443/')
+    }
 
-    dataService.setBaseServiceURL('https://desk-it.com:9443/')
+    //dataService.setBaseServiceURL('https://desk-it.com:9443/')
     //dataService.setBaseServiceURL('http://192.168.1.50:9000/')
 
 
