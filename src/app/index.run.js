de739ec24f3f531f6e6426e9c6de70e0b8bfaa30
paulsmal <pavel.smal@gmail.com>
2016-07-11 15:26:58 +0300

    backend url according to env set fix

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -12,13 +12,13 @@
     if (window.location.protocol === "https:" && window.location.host === "desk-it.com") {
       dataService.setBaseServiceURL('https://desk-it.com:9443/');
     } else if (window.location.protocol === "https:" && window.location.host === "uat.desk-it.com") {
-      dataService.setBaseServiceURL('https://uat.desk-it.com:10443/');
+      dataService.setBaseServiceURL('https://desk-it.com:10443/');
     } else if (window.location.protocol === "http:" && window.location.host === "desk-it.com") {
       dataService.setBaseServiceURL('http://desk-it.com:9000/');
     } else if (window.location.protocol === "http:" && window.location.host === "uat.desk-it.com") {
-      dataService.setBaseServiceURL('http://uat.desk-it.com:10000/');
+      dataService.setBaseServiceURL('http://desk-it.com:10000/');
     } else {
-      dataService.setBaseServiceURL('https://desk-it.com:9443/')
+      dataService.setBaseServiceURL('https://desk-it.com:10443/');
     }
 
     //dataService.setBaseServiceURL('https://desk-it.com:9443/')
