8b3bff4ad26215650137ac10e855ce8cece768da
Danil Golovachev <danithecake@gmail.com>
2016-07-24 03:19:57 +0300

    Implement visual part of message-to-ticket form.

--- src/app/mail/mailboxAttachMsgsController.js
+++ src/app/mail/mailboxAttachMsgsController.js
@@ -2,15 +2,73 @@
 
 angular
   .module('inspinia')
-  .controller("MailboxAttachMsgsController", function($log, $scope, $rootScope, $state, $stateParams, $sanitize,
-        mailboxService, ticketService) {
+  // Directive for visual formating ticket status
+  // TODO: Move in tickets controller or in separate directive folder
+  .directive('ticketStatus', function() {
+    return {
+      restrict: 'A',
+      scope: {stat: '=ticketStatus'},
+      template: '<span class="label" ng-class="labelClass">{{ labelTitle | translate }}</span>',
+      link: function(scope) {
+        switch (scope.stat) {
+          case 1:
+            scope.labelTitle = 'New';
+            scope.labelClass = 'label-success';
+            break;
+          case 2:
+            scope.labelTitle = 'Open';
+            scope.labelClass = 'label-warning';
+            break;
+          case 3:
+            scope.labelTitle = 'Postponed';
+            scope.labelClass = 'label-info';
+            break;
+          case 4:
+            scope.labelTitle = 'Resolved';
+            scope.labelClass = 'label-primary';
+            break;
+          default:
+            break;
+        }
+      }
+    };
+  })
+  // Directive for visual formating ticket priority
+  // TODO: Move in tickets controller or in separate directive folder
+  .directive('ticketPriority', function() {
+    return {
+      restrict: 'A',
+      scope: {prior: '=ticketPriority'},
+      template: '<span class="label" ng-class="labelClass">{{ labelTitle | translate }}</span>',
+      link: function(scope) {
+        switch (scope.prior) {
+          case 0:
+            scope.labelTitle = 'Low';
+            scope.labelClass = 'label-success';
+            break;
+          case 1:
+            scope.labelTitle = 'Middle';
+            scope.labelClass = 'label-warning';
+            break;
+          case 2:
+            scope.labelTitle = 'High';
+            scope.labelClass = 'label-danger';
+            break;
+          default:
+            break;
+        }
+      }
+    };
+  })
+  .controller("MailboxAttachMsgsController", function($log, $scope, $state, $stateParams, mailboxService,
+        ticketService) {
     // View
     var vm = this;
 
     // View title
     $scope.mbox.header = {
       icon: 'fa-envelope-o',
-      title: 'Selected messages',
+      title: 'Messages to attach',
       search: {placeholder: 'Search tickets'},
       btn: {title: 'New ticket', icon: 'fa-plus'},
       state: 'ambox'
@@ -20,7 +78,6 @@ angular
     vm.msgs = mailboxService.getSelectedMsgs();
     // Tickets
     vm.tickets = [];
-    $log.log(mailboxService.getSelectedMsgs());
 
     // Get tickets list
     vm.getTickets = function getTickets() {
@@ -31,9 +88,33 @@ angular
       }, function() { $log.log("Could not get tickets"); });
     };
 
+    // Select ticket
+    vm.selectTicket = function selectTicket(ticket) { vm.ticketId = ticket.id; };
+
+    // Remove message from selected list
+    vm.removeMsg = function removeMsg() {
+      $state.go('index.mail.inbox', {mailboxId: $scope.mbox.mailboxId});
+    };
+
     // TODO: disable test data
-    for (var i = 1; i < 41; i++) { vm.tickets.push({id: '0000' + 19 + i, subject: 'Test ' + i}) }
+    // for (var i = 1; i < 41; i++) {
+    //   vm.tickets.push({
+    //     id: '0000' + 19 + i,
+    //     status: i % 2 === 0 ? 2 : 3,
+    //     priority: i % 2 === 0 ? 2 : 0,
+    //     title: 'Test ' + i,
+    //     project: 'Test project',
+    //     created: new Date(),
+    //     deadline: new Date()
+    //   })
+    // }
+    // vm.msgs = [{
+    //   sender: 'rowanemployeecomm@rowancompanies.com',
+    //   received: 1436547049000,
+    //   preview: 'Test preview',
+    //   id: 267
+    // }];
     // TODO: disable test data
 
-    // vm.getTickets();
+    vm.getTickets();
   });
