4c45a9b4392cab5e2b7d0221c4da7a7f2d74f569
Danil Golovachev <danithecake@gmail.com>
2016-09-16 17:40:22 +0300

    Add directive for status and priority in ticket creation form
    Remove temporary dropdowns and add crmTicketPriorityDropdown and
    crmTicketStatusDropdown directives.

--- src/app/tickets/controllers/TicketCreateCtrl.js
+++ src/app/tickets/controllers/TicketCreateCtrl.js
@@ -8,36 +8,6 @@ angular
 
     // Model
     vm.model = {subject: '', description: '',  status: 1, priority: 0};
-    // TODO: Move it to directive
-    // Ticket statuses
-    vm.statuses = {
-      active: 0,
-      list: [
-        {id: 1, title: 'New', color: 'btn-danger'},
-        {id: 2, title: 'In progress', color: 'btn-success'},
-        {id: 3, title: 'Postponed', color: 'btn-info'},
-        {id: 4, title: 'Closed', color: 'btn-primary'}
-      ]
-    };
-    // TODO: Move it to directive
-    // Ticket priority
-    vm.priority = {
-      active: 0,
-      list: [
-        {id: 0, title: 'Low', color: 'btn-default'},
-        {id: 1, title: 'Middle', color: 'btn-info'},
-        {id: 2, title: 'High', color: 'btn-warning'},
-        {id: 3, title: 'ASAP', color: 'btn-danger'}
-      ]
-    };
-    vm.setStatus = function setStatus(i) {
-      vm.statuses.active = i;
-      vm.model.status = vm.statuses.list[i].id;
-    };
-    vm.setPriority = function setPriority(i) {
-      vm.priority.active = i;
-      vm.model.priority = vm.priority.list[i].id;
-    };
 
     // Selected project
     vm.project = null;
