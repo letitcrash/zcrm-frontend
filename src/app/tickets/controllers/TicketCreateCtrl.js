fbdef95e4ed1e0976fdca538c3579c799358abab
Danil Golovachev <danithecake@gmail.com>
2016-09-16 18:35:00 +0300

    Better ticket creation handling in UI
    Added loading thobber while wating sever response and error bar if server will
    return error. Added redirection to detail page of newly created ticket.

--- src/app/tickets/controllers/TicketCreateCtrl.js
+++ src/app/tickets/controllers/TicketCreateCtrl.js
@@ -6,6 +6,10 @@ angular
     // View
     var vm = this;
 
+    // Loading statuses
+    // 0 - error, 1 - success, 2 - loading
+    vm.loadStats = {form: 1};
+
     // Model
     vm.model = {subject: '', description: '',  status: 1, priority: 0};
 
@@ -23,9 +27,18 @@ angular
 
     // Create ticket
     vm.createTicket = function createTicket() {
-      $log.log(vm.model);
+      vm.loadStats.form = 2;
       ticketsAPI.create(vm.project, vm.model).then(function(res) {
+        if (res.hasOwnProperty('id')) {
+          vm.loadStats.form = 1;
+          $state.go('^.detail', {ticketId: res.id});
+        } else {
+          vm.loadStats.form = 0;
+          $log.log(res);
+        }
+      }, function(res) {
+        vm.loadStats.form = 0;
         $log.log(res);
-      }, function(res) { $log.log(res); });
+      });
     };
   });
