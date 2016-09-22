c4af899c910a51c5a15e77d4eaad5e9d22ee1488
Danil Golovachev <danithecake@gmail.com>
2016-09-22 20:51:42 +0300

    Implement reworked design for ticket detail page

--- src/app/tickets/controllers/TicketDetailCtrl.js
+++ src/app/tickets/controllers/TicketDetailCtrl.js
@@ -2,89 +2,29 @@
 
 angular
   .module('inspinia')
-  .controller('TicketDetailCtrl', function($log, $state, $stateParams, $scope, ticketsAPI,
+  .controller('TicketDetailCtrl', function($log, $state, $stateParams, $scope, ticketsAPI, ticketModel,
         textFromHTMLFilter) {
     // View
     var vm = this;
 
     // Selected ticket ID
     vm.ticketId = $state.params.ticketId;
-    // Selected ticket
-    vm.ticket = null;
+
     // Tab panel
     vm.tabs = [
+      {icon: 'fa-file-text-o', title: 'Description', disabled: false, template: 'app/tickets/detail-descr.html'},
       {icon: 'fa-comments-o', title: 'Messages', disabled: true},
-      {icon: 'fa-envelope', title: 'E-mail', disabled: false, template: 'app/tickets/detail-email.html'},
-      {icon: 'fa-user', title: 'People', disabled: false, template: 'app/tickets/detail-people.html'},
+      // {icon: 'fa-envelope', title: 'E-mail', disabled: false, template: 'app/tickets/detail-email.html'},
+      // {icon: 'fa-user', title: 'People', disabled: false, template: 'app/tickets/detail-people.html'},
       {icon: 'fa-paperclip', title: 'Files', disabled: true},
       {icon: 'fa-history', title: 'History', disabled: true}
     ];
     vm.activeTab = 3;
-    // Models
-    vm.models = {clients: [], teams: [], agents: [], status: 1, priority: 0, descr: ''};
-    // Forms
-    vm.forms = {
-      clients: {
-        active: false,
-        clear: function() {
-          vm.forms.clients.active = false;
-          vm.models.clients = vm.ticket.requesters.slice();
-        }
-      },
-      teams: {
-        active: false,
-        clear: function() {
-          vm.forms.teams.active = false;
-          vm.models.teams = vm.ticket.teams.slice();
-        }
-      },
-      agents: {
-        active: false,
-        clear: function() {
-          vm.forms.agents.active = false;
-          vm.models.agents = vm.ticket.members.slice();
-        }
-      },
-      // TODO: Move to directive
-      status: {
-        active: false,
-        titles: {1: 'New', 2: 'In progress', 3: 'Postponed', 4: 'Closed'},
-        colors: {1: 'btn-danger', 2: 'btn-success', 3: 'btn-info', 4: 'btn-primary'},
-        change: function(i) {
-          var id = parseInt(i);
 
-          vm.forms.status.active = id !== vm.ticket.status ? true : false;
-          vm.models.status = id;
-        },
-        clear: function() {
-          vm.forms.status.active = false;
-          vm.models.status = vm.ticket.status;
-        }
-      },
-      // TODO: Move to directive
-      priority: {
-        active: false,
-        titles: {0: 'Low', 1: 'Middle', 2: 'High', 3: 'ASAP'},
-        colors: {0: 'btn-default', 1: 'btn-info', 2: 'btn-warning', 3: 'btn-danger'},
-        change: function(i) {
-          var id = parseInt(i);
+    // Model
+    vm.model = ticketModel.model;
+    ticketModel.clear();
 
-          vm.forms.priority.active = id !== vm.ticket.priority ? true : false;
-          vm.models.priority = id;
-        },
-        clear: function() {
-          vm.forms.priority.active = false;
-          vm.models.priority = vm.ticket.priority;
-        }
-      },
-      descr: {
-        active: false,
-        clear: function() {
-          vm.forms.descr.active = false;
-          vm.models.descr = vm.ticket.description;
-        }
-      }
-    };
     // Attached emails
     vm.emails = [];
 
@@ -92,13 +32,7 @@ angular
     vm.getTicket = function getTicket() {
       ticketsAPI.get(vm.ticketId).then(function(res) {
         $log.log(res);
-        vm.ticket = res;
-        vm.models.clients = vm.ticket.requesters.slice();
-        vm.models.teams = vm.ticket.teams.slice();
-        vm.models.agents = vm.ticket.members.slice();
-        vm.models.descr = vm.ticket.description;
-        vm.models.status = vm.ticket.status;
-        vm.models.priority = vm.ticket.priority;
+        vm.model = res;
       }, function() { $log.log('Couldn\'t get Ticket details'); });
     };
 
