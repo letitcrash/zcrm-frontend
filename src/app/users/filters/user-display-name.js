9d98af6fb49f974b2dad336a1f945bb9da659467
Danil Golovachev <danithecake@gmail.com>
2016-09-22 20:50:49 +0300

    Make crmUserDisplayName more robust

--- src/app/users/filters/user-display-name.js
+++ src/app/users/filters/user-display-name.js
@@ -5,13 +5,24 @@ angular
   // Filter for displaying users 'Firstname Lastname' or initials, or 'username' if he didn't sepcify his name.
   .filter('crmUserDisplayName', function() {
     return function(user, initialsOnly) {
-      var res = user.hasOwnProperty('username') ? user.username : '';
+      var res = '';
 
-      if (user.hasOwnProperty('contactProfile')) {
-        var pfile = user.contactProfile;
+      if (angular.isObject(user)) {
+        if (user.hasOwnProperty('username'))
+          res = user.username;
 
-        if (pfile.hasOwnProperty('firstname') || pfile.hasOwnProperty('lastname'))
-          res = initialsOnly ?  pfile.firstname[0] + pfile.lastname[0] : pfile.firstname + ' ' + pfile.lastname;
+        if (user.hasOwnProperty('contactProfile')) {
+          var pfile = user.contactProfile;
+          var parts = [];
+
+          if (pfile.hasOwnProperty('firstname'))
+            parts.push(initialsOnly ?  pfile.firstname[0] : pfile.firstname);
+
+          if (pfile.hasOwnProperty('lastname'))
+            parts.push(initialsOnly ?  pfile.lastname[0] : pfile.lastname);
+
+          res = parts.join(' ');
+        }
       }
 
       return res;
