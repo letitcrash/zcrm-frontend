a300c0f1db6d01e34e884690dac8319b73de0d4d
Danil Golovachev <danithecake@gmail.com>
2016-10-04 03:46:31 +0300

    Add model for Employee additional fields

--- src/app/employees/services/employee-model.js
+++ src/app/employees/services/employee-model.js
@@ -13,6 +13,29 @@ angular
       this.phoneNumberMobile = '';
     }
 
+    // TODO: Delete after backend refactoring
+    function EmpAdditionalInfo() {
+      this.membershipNum = null;
+      this.enrolledDept = null;
+      this.enrolledIE = null;
+      this.enrolledLO = null;
+      this.refBy = '';
+      this.resignedDate = null;
+      this.shopStewardCourses = null;
+      this.agreement = '';
+      this.voCourses = null;
+      this.emplNumber = null;
+      this.status = '';
+      this.workplace = '';
+      this.comment = '';
+      this.land = false;
+      this.seniorityDate = null;
+      this.socialSecNumber = null;
+      this.jacketSize = '';
+      this.indivPaid = false;
+      this.airport = '';
+    }
+
     function Employee() {
       this.username = '';
       this.baseUrl = '';
@@ -26,6 +49,7 @@ angular
       this.flypass = '';
 
       this.contactProfile = new ContactProfile();
+      this.additionalInfo = new EmpAdditionalInfo();
     }
 
     return {
