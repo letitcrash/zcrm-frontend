0c8822c50b7a91559e00126199da4d1c51d415b0
Mykyta Nikitenko <nikitanikitenko@gmail.com>
2016-10-05 12:32:42 +0300

    Fixing translation CRM-84 CRM-88

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -2,45 +2,42 @@
 
 angular
   .module('inspinia')
-  .config(Decorate);
+  .config(function ($provide) {
+    var attr = 'crm-select-multiple';
 
-function Decorate($provide) {
-  var attr = 'crm-select-multiple';
+    // ui-select decorator with custom template
+    $provide.decorator('uiSelectDirective', function ($delegate) {
+      var selectMatch = $delegate[0];
+      var tempFunc = selectMatch.templateUrl;
 
-  // ui-select decorator with custom template
-  $provide.decorator('uiSelectDirective', function($delegate) {
-    var selectMatch = $delegate[0];
-    var tempFunc = selectMatch.templateUrl;
+      selectMatch.templateUrl = function (elem, atts) {
 
-    selectMatch.templateUrl = function(elem, atts) {
+        if (elem[0].hasAttribute(attr) || elem[0].hasAttribute('data-' + attr))
+          return 'app/common/directives/select-multiple.html';
 
-      if (elem[0].hasAttribute(attr) || elem[0].hasAttribute('data-' + attr)) {
-        return 'app/common/directives/select-multiple.html';
-      } else {
         return tempFunc(elem, atts);
-      }
-    };
+      };
 
-    return $delegate;
-  });
+      return $delegate;
+    });
 
-  // ui-select-match decorator with custom template
-  $provide.decorator('uiSelectMatchDirective', function($delegate) {
-    var selectMatch = $delegate[0];
-    var tempFunc = selectMatch.templateUrl;
+    // ui-select-match decorator with custom template
+    $provide.decorator('uiSelectMatchDirective', function ($delegate) {
+      var selectMatch = $delegate[0];
+      var tempFunc = selectMatch.templateUrl;
 
-    selectMatch.templateUrl = function(elem, atts) {
-      var parent = elem.parent()[0];
+      selectMatch.templateUrl = function (elem, atts) {
+        var parent = elem.parent()[0];
 
-      if (parent.hasAttribute(attr) || parent.hasAttribute('data-' + attr)) {
-        elem.addClass('ui-select-match');
+        if (parent.hasAttribute(attr) || parent.hasAttribute('data-' + attr)) {
+          elem.addClass('ui-select-match');
 
-        return 'app/common/directives/select-multiple-match.html';
-      } else {
+          return 'app/common/directives/select-multiple-match.html';
+        }
+
         return tempFunc(elem, atts);
-      }
-    };
+      };
 
-    return $delegate;
+      return $delegate;
+    });
   });
-}
