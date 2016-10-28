8e3349f3b8d87e0188f5e05b972b97ceb792e163
Mykyta Nikitenko <nikitanikitenko@gmail.com>
2016-10-28 14:35:21 +0300

    Refactor of deletion confirmation in cms

--- src/app/cms/directives/delete.js
+++ src/app/cms/directives/delete.js
@@ -2,7 +2,7 @@
 
 angular
   .module('inspinia')
-  .directive('crmDelete', function () {
+  .directive('cmsDelete', function () {
     return {
       templateUrl: 'app/cms/directives/delete.html'
     };
